1b488e15006a67005c6b363e76968a95bf218510
Peter Gasparik <gasparik.pe@gmail.com>
2020-06-11 19:08:58 +0200

    build and deploy

--- dist/compare/PropertyComparable.d.ts
+++ dist/compare/PropertyComparable.d.ts
@@ -1,6 +1,6 @@
 import { Comparable } from "./Comparable";
 export declare class PropertyComparable implements Comparable<any> {
-    private propertyName;
+    private propertyAdapter;
     constructor(propertyName: string);
     compare(a: any, b: any): number;
 }
--- dist/compare/PropertyComparable.js
+++ dist/compare/PropertyComparable.js
@@ -1,14 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var ObjectPropertyAdapter_1 = require("../repository/adapter/ObjectPropertyAdapter");
 var PropertyComparable = (function () {
     function PropertyComparable(propertyName) {
-        this.propertyName = propertyName;
+        this.propertyAdapter = new ObjectPropertyAdapter_1.ObjectPropertyAdapter(propertyName);
     }
     PropertyComparable.prototype.compare = function (a, b) {
-        if (a[this.propertyName] < b[this.propertyName]) {
+        var aValue = this.propertyAdapter.adapt(a);
+        var bValue = this.propertyAdapter.adapt(b);
+        if (aValue < bValue) {
             return -1;
         }
-        else if (a[this.propertyName] > b[this.propertyName]) {
+        else if (aValue > bValue) {
             return 1;
         }
         return 0;
